64568f784cf1e11f2941be29fcbffcb83c3a5e69
Rafael Menezes Barboza <rbarboza@alunos.utfpr.edu.br>
2017-11-30 17:07:24 -0200

    Add files via upload

--- script.sh
+++ script.sh
@@ -1,161 +1,70 @@
-#!/bin/sh
+#!/bin/bash
 
-echo “========================================”
+echo "----------------------------"
+echo "REDES I COM MININET 2017"
+echo "----------------------------"
+echo "Rafael Menezes RA:1817485"
+echo "Jonas Felipe   RA:1817450"
+echo "----------------------------"
 
-echo “Configuração automática de servidor DHCP”
 
-echo “========================================”
+echo "Insira o numero de maquinas na rede"
+read qtd_PC
 
-echo “”
+echo "Instalar DHCPD-SERVER? (Y/N)"
+read conf
 
-echo “Verificando se o servidor dhcp está instalado…”
+#Se minha resposta for Sim! vou instalar DHCPD-Server
+if [conf == Y]{
+	apt-get install dhcp3-server
+}
 
-DHCPOK=$(dpkg -l | grep isc-dhcp-server)
+echo "Instalar Interface Xinit Flwm? (Y/N)"
+read conf
 
-if [ -z $DHCPOK ]; then
+#Se minha resposta for Sim! vou instalar DHCPD-Server
+if [conf == Y]{
+	apt-get install xinit flwm
+	startx
+}
 
-echo “Instalando isc-dhcp-server…”
+echo "----------------------------"
+echo "Configurando DHCP.CONF"
+echo "----------------------------"
 
-apt-get install isc-dhcp-server > /dev/null 2>&1
+echo "Insira o IP do servidor Principal:"
+read ipServer
 
-if [ $? -eq 0 ]; then
+echo "Insira a Mascara de Rede:"
+read mask
 
-“OK!”
+echo "Insira o range(1) de IPs da Rede:"
+read range1
 
-else
+echo "Insira o range(2) de IPs da Rede:"
+read range2
 
-echo “Não foi possível instalar o serviço isc-dchp-server!”
+echo "Insira o endereço de broadcast:"
+read broadcast
 
-echo “FIM SCRIPT”
+#Configurando subnet
+echo subnet $ipServer netmask $mask {
+		range $range1 $range2;
+		option broadcast-address $broadcast;
+	} >>  /etc/dhcp/dhcpd.conf
 
-fi
+echo "----------------------------"
+echo "Criando topologia"
+echo "----------------------------"
 
-else
+echo "Insira o numero de Maquinas na Rede:"
+read qtd_PC
 
-echo “OK!”
+#Cria uma topologia 1 SW e qtd_PC Computadores e faz os LINKES
+mn --topo=single,qtd_PC 
 
-fi
+echo "Comando magico"
+ifconfig h1-eth0 ipServer
 
-# Configurando a interface de rede
-
-# Obtendo dados para criar o arquivo de configuração:
-
-echo “Digite o endereço da rede:”
-
-read SUBNET
-
-echo “Digite a máscara da rede:”
-
-read NETMASK
-
-echo “Digite o range de endereços disponíveis na rede separados por vírgula e sem espaço!”
-
-read NETRANGE
-
-NETRANGE=$(echo $NETRANGE | sed -e ‘s/,/ /’)
-
-echo “Informe os endereços de DNS separados por virgula sem espaço!”
-
-read DNS
-
-echo “”
-
-IPGW=$(ifconfig eth0 | grep Bcast | awk -F” ” ‘{print $2}’ | awk -F: ‘{print $2’})
-
-echo “Construino arquivo de configuração…”
-
-# Construindo arquivo de configuração
-
-echo “ddns-update-style none;” > dhcpconfig.auto
-
-echo ” subnet $SUBNET netmask $NETMASK {” >> dhcpconfig.auto
-
-echo ” range dynamic-bootp $NETRANGE;” >> dhcpconfig.auto
-
-echo “” >> dhcpconfig.auto
-
-echo ” option routers $IPGW;” >> dhcpconfig.auto
-
-echo ” option subnet-mask $NETMASK;” >> dhcpconfig.auto
-
-echo ” option domain-name-servers $DNS;” >> dhcpconfig.auto
-
-echo ” default-lease-time 21600;” >> dhcpconfig.auto
-
-echo ” max-lease-time 43200;” >> dhcpconfig.auto
-
-echo “” >> dhcpconfig.auto
-
-echo ” # COMPUTADORES COM IP FIXO:” >> dhcpconfig.auto
-
-# Inserindo IPs fixos
-
-OP=”s”
-
-while [ $OP = s ]; do
-
-echo “Deseja inserir algum IP fixo? (s/N)”
-
-read OP
-
-OP=$(echo $OP | tr [[:upper:]] [[:lower:]])
-
-if [ $OP = s ]; then
-
-echo “Digite o nome do host:”
-
-read HOST
-
-echo “Digite o endereço MAC:”
-
-read MAC
-
-echo “Digite o número de IP:”
-
-read IP
-
-echo ” host $HOST {” >> dhcpconfig.auto
-
-echo ” hardware ethernet $MAC;” >> dhcpconfig.auto
-
-echo ” fixed-address $IP;” >> dhcpconfig.auto
-
-echo ” }” >> dhcpconfig.auto
-
-fi
-
-done
-
-# Finalizando o arquivo de configuração
-
-echo “”
-
-echo ” }” >> dhcpconfig.auto
-
-echo “Finalizando o arquivo de configuração…”
-
-sleep 5
-
-echo “O arquivo de configuração foi criado em $(pwd)/dhcpconfig.auto”
-
-# Substituindo o arquivo de configuração original
-
-echo “Deseja substituir o arquivo /etc/dhcp/dhcpd.conf? (s/N)”
-
-read OP
-
-OP=$(echo $OP | tr [[:upper:]] [[:lower:]])
-
-if [ $OP = s ]; then
-
-mv /etc/dhcp/dhcpd.conf /etc/dhcp/dhcpd.conf.bkp
-
-mv $(pwd)/dhcpconfig.auto /etc/dhcp/dhcpd.conf
-
-fi
-
-# Reiniciando o serviço
-
-echo “Reiniciando o servdidor dhcp…”
-
-invoke-rc.d isc-dhcp-server restart
+echo "Startando o dhcp"
+dhcpd -cf /etc/dhcp/dhcpd.conf
